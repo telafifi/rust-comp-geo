2b3776a7bd21a47c4f126714019245b4073a8f97
Tarek <telafifi@gmail.com>
2024-12-24 23:58:37 -0600

    Update test for geometry extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod geometry;
+use crate::geometry::point::{p2p_dist, create_point};
+use crate::geometry::types::Point;
+
 fn main() {
-    println!("Hello, world!");
+    let p1: Point = create_point(4.0, 3.0);
+    let p2: Point = create_point(6.0, 8.0);
+    let dist: f64 = p2p_dist(p1, p2);
+    println!("Distance from origin: {}", dist);
 }
---
+++ target/debug/deps/rust_comp_geo-4c042552c92d55ae.d
@@ -0,0 +1,8 @@
+/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/librust_comp_geo-4c042552c92d55ae.rmeta: src/main.rs src/geometry/mod.rs src/geometry/point.rs src/geometry/types.rs
+
+/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/rust_comp_geo-4c042552c92d55ae.d: src/main.rs src/geometry/mod.rs src/geometry/point.rs src/geometry/types.rs
+
+src/main.rs:
+src/geometry/mod.rs:
+src/geometry/point.rs:
+src/geometry/types.rs:
