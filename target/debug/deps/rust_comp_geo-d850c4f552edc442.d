dc91b96b12cdf6e104f9dc6726c3a477fc5a7854
Tarek <telafifi@gmail.com>
2024-12-25 16:07:52 -0600

    remove unused

--- target/debug/deps/rust_comp_geo-d850c4f552edc442.d
+++
@@ -1,8 +0,0 @@
-/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/rust_comp_geo-d850c4f552edc442: src/main.rs src/geometry/mod.rs src/geometry/point.rs src/geometry/types.rs
-
-/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/rust_comp_geo-d850c4f552edc442.d: src/main.rs src/geometry/mod.rs src/geometry/point.rs src/geometry/types.rs
-
-src/main.rs:
-src/geometry/mod.rs:
-src/geometry/point.rs:
-src/geometry/types.rs:
--- target/debug/deps/rust_comp_geo-f88de5921cc0881e.d
+++
@@ -1,8 +0,0 @@
-/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/librust_comp_geo-f88de5921cc0881e.rmeta: src/main.rs src/geometry/mod.rs src/geometry/point.rs src/geometry/types.rs
-
-/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/rust_comp_geo-f88de5921cc0881e.d: src/main.rs src/geometry/mod.rs src/geometry/point.rs src/geometry/types.rs
-
-src/main.rs:
-src/geometry/mod.rs:
-src/geometry/point.rs:
-src/geometry/types.rs:
