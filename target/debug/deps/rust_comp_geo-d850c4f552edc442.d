18a2173a51931ab2886cb69586f9a5854cd32e5a
Tarek <telafifi@gmail.com>
2024-12-24 22:28:10 -0600

    Hello world with rust

---
+++ target/debug/deps/rust_comp_geo-d850c4f552edc442.d
@@ -0,0 +1,5 @@
+/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/rust_comp_geo-d850c4f552edc442: src/main.rs
+
+/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/deps/rust_comp_geo-d850c4f552edc442.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-comp-geo.d
@@ -0,0 +1,1 @@
+/Users/telafifi/Sites/adventures/rust-comp-geo/target/debug/rust-comp-geo: /Users/telafifi/Sites/adventures/rust-comp-geo/src/main.rs
