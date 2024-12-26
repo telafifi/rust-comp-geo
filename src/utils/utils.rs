cb980e57e22f9ee58dc97af9c1592285e06d9fe7
Tarek <telafifi@gmail.com>
2024-12-25 22:57:30 -0600

    add rounding as a test function

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,3 +1,21 @@
+/**
+ * Check if two numbers are close to each other, under a certain tolerance.
+ */
 pub fn close_equal(a: f64, b: f64, tolerance: Option<f64>) -> bool {
     (a - b).abs() < tolerance.unwrap_or(0.000001)
+}
+
+/**
+ * Round a number to n decimal places.
+ */
+pub fn round_to_n_decimals(x: f64, n: u32) -> f64 {
+    let factor = 10_f64.powi(n as i32);
+    (x * factor).round() / factor
+}
+
+/**
+ * Round a number to 6 decimal places.
+ */
+pub fn round(x: f64) -> f64 {
+    round_to_n_decimals(x, 6)
 }
--- tests/utils/utils_test.rs
+++ tests/utils/utils_test.rs
@@ -1,4 +1,4 @@
-use rust_comp_geo::utils::utils::close_equal;
+use rust_comp_geo::utils::utils::{close_equal, round};
 
 #[cfg(test)] // Only compiles when running tests
 mod close_equal_test {
@@ -33,4 +33,27 @@ mod close_equal_test {
       let eq = close_equal(10.0, 9.998, Some(0.1));
       assert!(eq);
   }
+
+  #[test]
+  fn round_close_equal_test() {
+    let test_cases: Vec<f64> = vec![
+        4.815162342,
+        9.999999,
+        2.0,
+        -7.0,
+        12345.6789,
+        0.0,
+        -0.00000001,
+    ];
+
+
+    // Test that the rounded value is close to the original value
+    // for the entirety of the list.
+    for x in test_cases {
+        let rounded = round(x);
+        let eq = close_equal(x, rounded, None);
+        assert!(eq);
+    }
+  }
+
 }
